58bac4a402bc3556b19eb65313877a9ea8e9f41f
Philip Pryce <philip.pryce@me.com>
2026-03-02 20:28:37 +0000

    Optimise storing counts to flat array
    Rather than storing path -> date -> count, simply store both path and
    date as integers and separate them out later.
    
    Sample a chunk of the input file to determine the order the paths appear
    in for the first time, so the data is pre-sorted for us.

--- app/Parser.php
+++ app/Parser.php
@@ -22,12 +22,10 @@ final class Parser
         $pathCache = [];
         $pathByIds = [];
 
-        foreach (Visit::all() as $pathId => $visit) {
-            $path = \substr($visit->uri, $prefixLength);
-            $pathCache[$path] = $pathId;
-            $pathByIds[$pathId] = $path;
-        }
+        $pathIdOffset = 0;
+        $pathIdCounter = 0;
 
+        $dateIdCounter = 0;
         for ($year = 2021; $year <= 2026; $year++) {
             for ($month = 1; $month <= 12; $month++) {
                 $yearMonth = \sprintf('%d-%02d-', $year, $month);
@@ -40,11 +38,44 @@ final class Parser
 
                 for ($day = 1; $day <= $days; $day++) {
                     $rawDate = $yearMonth . \sprintf('%02d', $day);
-                    $parsedDateCache[$rawDate] = (int) ($year . $month . $day);
+                    $parsedDateCache[$rawDate] = $dateIdCounter++;
                     $formattedDatesByInt[$parsedDateCache[$rawDate]] = $rawDate;
+                    $pathIdOffset++;
                 }
             }
         }
+
+        $totalUniquePaths = count(Visit::all());
+
+        $sample = \fread($fh, $chunkSize / 2);
+        $offset = 0;
+        while (true) {
+            $newlinePos = \strpos($sample, "\n", $offset);
+
+            if ($newlinePos === false) {
+                break;
+            }
+
+            $commaPos = $newlinePos - $commaOffsetFromNewline;
+            $pathStart = $offset + $prefixLength;
+            $path = \substr($sample, $pathStart, $commaPos - $pathStart);
+
+            if (! isset($pathCache[$path])) {
+                $pathCache[$path] = $pathId = $pathIdCounter++ * $pathIdOffset;
+                $pathByIds[$pathId] = $path;
+                foreach ($formattedDatesByInt as $dateId => $_) {
+                    $result[$pathId + $dateId] = 0;
+                }
+
+                if ($pathIdCounter === $totalUniquePaths) {
+                    break;
+                }
+            }
+
+            $offset = $newlinePos + 1;
+        }
+
+        fseek($fh, 0);
 
         while (! \feof($fh)) {
             $chunk = \fread($fh, $chunkSize);
@@ -69,16 +100,9 @@ final class Parser
                 $rawDate = \substr($buffer, $commaPos + 1, 10);
 
                 $pathId = $pathCache[$path];
+                $dateId = $parsedDateCache[$rawDate];
 
-                $date = $parsedDateCache[$rawDate];
-
-                $pathDates = &$result[$pathId];
-
-                if (isset($pathDates[$date])) {
-                    $pathDates[$date]++;
-                } else {
-                    $pathDates[$date] = 1;
-                }
+                $result[$pathId + $dateId]++;
 
                 $offset = $newlinePos + 1;
             }
@@ -94,69 +118,48 @@ final class Parser
             $rawDate = \substr($buffer, $commaPos + 1, 10);
 
             $pathId = $pathCache[$path];
+            $dateId = $parsedDateCache[$rawDate];
 
-            $date = $parsedDateCache[$rawDate];
-
-            $pathDates = &$result[$pathId];
-
-            if (isset($pathDates[$date])) {
-                $pathDates[$date]++;
-            } else {
-                $pathDates[$date] = 1;
-            }
+            $result[$pathId + $dateId]++;
         }
 
         $out = \fopen($outputPath, 'wb');
 
         $outBuffer = "{\n";
         $outBufferFlushSize = 1024 * 1024;
-        $isFirstPath = true;
 
-        foreach ($result as $pathId => $dates) {
+        $lastPathId = null;
+
+        foreach ($result as $key => $count) {
+            if ($count === 0) {
+                continue;
+            }
+
+            $dateId = $key % $pathIdOffset;
+            $pathId = $key - $dateId;
+
             $path = $pathByIds[$pathId];
 
-            if ($isFirstPath) {
-                $isFirstPath = false;
+            if ($lastPathId === null) {
+                // 1st path
+                $outBuffer .= '    "\/blog\/' . $path . "\": {\n";
+                $lastPathId = $pathId;
+            } elseif ($lastPathId !== $pathId) {
+                // Nth path
+                $outBuffer .= "\n    },\n" . '    "\/blog\/' . $path . "\": {\n";
+                $lastPathId = $pathId;
             } else {
+                // Trailing comma for count
                 $outBuffer .= ",\n";
             }
 
-            if (\count($dates) === 1) {
-                $outBuffer .= '    "\/blog\/' . $path . "\": {\n";
-
-                foreach ($dates as $date => $count) {
-                    $outBuffer .= '        "' . $formattedDatesByInt[$date] . '": ' . $count;
-                }
-
-                $outBuffer .= "\n    }";
-            } else {
-                \ksort($dates, \SORT_NUMERIC);
-
-                $outBuffer .= '    "\/blog\/' . $path . "\": {\n";
-
-                $isFirstDate = true;
-
-                foreach ($dates as $date => $count) {
-                    if ($isFirstDate) {
-                        $isFirstDate = false;
-                    } else {
-                        $outBuffer .= ",\n";
-                    }
-
-                    $outBuffer .= '        "' . $formattedDatesByInt[$date] . '": ' . $count;
-                }
-
-                $outBuffer .= "\n    }";
-            }
-
+            $outBuffer .= '        "' . $formattedDatesByInt[$dateId] . '": ' . $count;
             if (\strlen($outBuffer) >= $outBufferFlushSize) {
                 \fwrite($out, $outBuffer);
                 $outBuffer = '';
             }
         }
 
-        $outBuffer .= "\n}";
-
-        \fwrite($out, $outBuffer);
+        \fwrite($out, $outBuffer . "\n    }\n}");
     }
 }
